5bb401e333ff8177eda47928e62bbbe1405a48ac
Dipesh Rijal <dipesh.rijal@gmail.com>
2017-11-21 11:55:00 -0600

    session

--- src/main/java/edu/mum/domain/Session.java
+++ src/main/java/edu/mum/domain/Session.java
@@ -1,6 +1,9 @@
 package edu.mum.domain;
 
 import java.io.Serializable;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -9,6 +12,8 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
 import org.hibernate.validator.constraints.NotEmpty;
 
