32627f022620b3bb9e4ed96ad3f1002cb2efaa08
Dipesh Rijal <dipesh.rijal@gmail.com>
2017-11-22 04:28:00 -0600

    past session cannot be booked

--- src/main/java/edu/mum/controller/AppointmentController.java
+++ src/main/java/edu/mum/controller/AppointmentController.java
@@ -1,6 +1,7 @@
 package edu.mum.controller;
 
 import java.security.Principal;
+import java.util.Date;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
@@ -13,9 +14,11 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import edu.mum.domain.Appointment;
 import edu.mum.domain.Person;
+import edu.mum.domain.Session;
 import edu.mum.service.AppointmentService;
 import edu.mum.service.PersonService;
 import edu.mum.service.SessionService;
@@ -40,7 +43,7 @@ public class AppointmentController {
 
 		return "appointments/index";
 	}
-	
+
 	@RequestMapping(value = "/all", method = RequestMethod.GET)
 	public String listAllAppointments(Model model) {
 		model.addAttribute("appointments", appointmentService.findAll());
@@ -53,7 +56,7 @@ public class AppointmentController {
 		appointmentService.deleteById(id);
 
 		String referer = request.getHeader("Referer");
-		
+
 		return "redirect:" + referer;
 
 		// return "redirect:/appointments";
@@ -68,10 +71,19 @@ public class AppointmentController {
 
 	@RequestMapping(value = "/create", method = RequestMethod.POST)
 	public String processAddNewAppointmentForm(@ModelAttribute("appointment") @Valid Appointment appointment,
-			BindingResult result, Principal principal, Model model) {
+			BindingResult result, Principal principal, Model model, RedirectAttributes redirectAttrs) {
 		model.addAttribute("sessions", sessionService.findAll());
 		if (result.hasErrors()) {
 			return "appointments/create";
+		}
+		
+		Session session = sessionService.findOne(appointment.getSession().getId());
+		
+		
+		
+		if (session.getDate().before(new Date())) {
+			redirectAttrs.addFlashAttribute("message", "You cannot book the past session.");
+			return "redirect:/appointments/create";
 		}
 
 		Person customer = personService.findByUsername(principal.getName());
