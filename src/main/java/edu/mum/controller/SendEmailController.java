ef510cee5ee95148ed10d3160e4830b65e12e188
unknown <yaman_upadhayay@yahoo.com>
2017-11-21 23:19:30 -0600

    Email Integration - First

---
+++ src/main/java/edu/mum/controller/SendEmailController.java
@@ -0,0 +1,41 @@
+package edu.mum.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class SendEmailController {
+	
+	@Autowired
+    private JavaMailSender mailSender;
+     
+    @RequestMapping(value="/mail",method = RequestMethod.GET)
+    public void doSendEmail() {
+        // takes input from e-mail form
+        String recipientAddress = "yaman_upadhaya@yahoo.com";
+        String subject = "Test Email";
+        String message = "Message successfully recieved...!";
+         
+        // prints debug info
+        System.out.println("To: " + recipientAddress);
+        System.out.println("Subject: " + subject);
+        System.out.println("Message: " + message);
+         
+        // creates a simple e-mail object
+        SimpleMailMessage email = new SimpleMailMessage();
+        email.setTo(recipientAddress);
+        email.setSubject(subject);
+        email.setText(message);
+         
+        // sends the e-mail
+        mailSender.send(email);
+         
+        // forwards to the view named "Result"
+        //        return "Result";
+    }
+
+}
